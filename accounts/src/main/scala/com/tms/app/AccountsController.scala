e07fc8ff22afcdfe867b05dda397806cfc17524f
Luis Palomino Trevilla <luispalominot3@gmail.com>
2019-05-01 20:03:45 -0500

    Test to return json

--- accounts/src/main/scala/com/tms/app/AccountsController.scala
+++ accounts/src/main/scala/com/tms/app/AccountsController.scala
@@ -1,11 +1,22 @@
 package com.tms.app
 
 import org.scalatra._
+import org.json4s.{DefaultFormats, Formats}
+import org.scalatra.json._
 
-class AccountsController extends ScalatraServlet {
+class AccountsController extends ScalatraServlet with JacksonJsonSupport {
+
+  protected implicit lazy val jsonFormats: Formats = DefaultFormats
+
+  before() {
+    contentType = formats("json")
+  }
 
   get("/") {
-    "Hola Mundo"
+    val account = Account("luispalominot@hotmail.com", "Luis Palomino", "luismfao2")
+    account
   }
 
 }
+
+case class Account(email: String, name: String, password: String)
