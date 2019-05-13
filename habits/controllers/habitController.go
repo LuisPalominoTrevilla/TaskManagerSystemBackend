59f30cd6e1a06a20cabdc4aceff58dbafacb11f6
FridaADM <fridaadm@gmail.com>
2019-05-13 18:03:32 -0500

    added endpoint

--- habits/controllers/habitController.go
+++ habits/controllers/habitController.go
@@ -1,26 +1,81 @@
 package controllers
-
 import (
 	"fmt"
 	"net/http"
-
+	"encoding/json"
+	"strconv"
+	"os"
+	"strings"
+	"io"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"github.com/mongodb/mongo-go-driver/bson"
 	database "github.com/LuisPalominoTrevilla/TaskManagerSystemBackend/db"
+	"github.com/LuisPalominoTrevilla/TaskManagerSystemBackend/models"
 	"github.com/mongodb/mongo-go-driver/mongo"
 	"github.com/gorilla/mux"
-)
 
+)
 // HabitsController holds important data for the habits controller
 type HabitsController struct {
 	habitsDB *database.HabitsDB
 }
-
-// Get serves as a GET request
+// Get serves as a GET request for either all or one specific habit
 func (controller *HabitsController) Get(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Habits controller")
+	vars := mux.Vars(r)
+	id := vars["id"]
+	if len(id) < 1 {
+		habits := controller.getAll()
+		if habits == nil {
+			w.WriteHeader(500)
+			fmt.Fprint(w, "Error retrieving habits from database.")
+			return
+		}
+		response := struct {
+			Habits []*models.Habit
+		} {
+			habits,
+		}
+		w.Header().Add("Content-Type", "application/json")
+		encoder := json.NewEncoder(w)
+		encoder.Encode(response)
+        return
+	}
+	habitID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		println(err.Error())
+		w.WriteHeader(500)
+		fmt.Fprint(w, "Error while intepreting the habit ID.")
+		return
+	}
+	filter := bson.D{{"_id", habitID}}
+	var habit models.Habit
+	err2 := controller.habitsDB.GetByID(filter, &habit)
+	if(err2 != nil) {
+		println(err2.Error())
+		w.WriteHeader(500)
+		fmt.Fprint(w, "Error retrieving habit from database.")
+		return
+	}
+	w.Header().Add("Content-Type", "application/json")
+	encoder := json.NewEncoder(w)
+	encoder.Encode(habit)
 }
-
+func (controller *HabitsController) getAll() []*models.Habit {
+	var results []*models.Habit
+	filter := bson.D{}
+	results, err := controller.habitsDB.Get(filter)
+	if(err != nil) {
+		results = nil
+	}
+	return results
+}
 func (controller *HabitsController) initializeController(r *mux.Router) {
+	r.HandleFunc("/{id}", controller.Get).Methods(http.MethodGet)
 	r.HandleFunc("/", controller.Get).Methods(http.MethodGet)
+	r.HandleFunc("/", controller.CreateHabit).Methods(http.MethodPost)
+	r.HandleFunc("/{id}", controller.EditHabit).Methods(http.MethodPut)
+	r.HandleFunc("/{id}", controller.DeleteHabit).Methods(http.MethodDelete)
+	r.HandleFunc("/{id}", controller.SumPoints).Methods(http.MethodGet)
 }
 
 // SetHabitsController sets the controller for the sets up the habits controllet
@@ -29,4 +84,326 @@ func SetHabitsController(r *mux.Router, db *mongo.Database) {
 	
 	habitsController := HabitsController{habitsDB: &habit}
 	habitsController.initializeController(r)
+}
+// CreateHabit serves to receive a POST request to create a new habit
+func (controller *HabitsController) CreateHabit(w http.ResponseWriter, r *http.Request) {
+	var maxBytes int64 = 64 * 1024 * 1024
+	validImageFormats := map[string]bool{
+		"image/png":  true,
+		"image/jpeg": true,
+	}
+	// Parse multipart form data
+	err := r.ParseMultipartForm(maxBytes)
+	if err != nil {
+		println(err.Error())
+		w.WriteHeader(500)
+		fmt.Fprint(w, "Error while intepreting the data provided.")
+		return
+	}
+	// Ensure that both the image and age are contained in multipartform
+	if len(r.MultipartForm.File["image"]) == 0 {
+		w.WriteHeader(400)
+		fmt.Fprint(w, "Missing image.")
+		return
+	}
+	if len(r.MultipartForm.Value["title"]) == 0 {
+		w.WriteHeader(400)
+		fmt.Fprint(w, "Missing title.")
+		return
+	}
+	if len(r.MultipartForm.Value["type"]) == 0 {
+		w.WriteHeader(400)
+		fmt.Fprint(w, "Missing type.")
+		return
+	}
+	if len(r.MultipartForm.Value["difficulty"]) == 0 {
+		w.WriteHeader(400)
+		fmt.Fprint(w, "Missing difficulty.")
+		return
+	}
+	// get userEmail from header
+	userId := r.Header.Get("userId")
+	// get age from image
+	hType, err := strconv.Atoi(r.MultipartForm.Value["type"][0])
+	if (err != nil || hType < -1 || hType > 1){
+		println(err.Error())
+		w.WriteHeader(400)
+		fmt.Fprint(w, "Either type was not a number or is not within the permitted range.")
+		return
+	}
+	difficulty, err := strconv.Atoi(r.MultipartForm.Value["difficulty"][0])
+	if (err != nil){
+		println(err.Error())
+		w.WriteHeader(400)
+		fmt.Fprint(w, "Difficulty was not a number.")
+		return
+	}
+	// get image file header
+	imFileHeader := r.MultipartForm.File["image"][0]
+	im, err := imFileHeader.Open()
+	
+	if err != nil {
+		println(err.Error())
+		w.WriteHeader(500)
+		fmt.Fprint(w, "Error opening the image file.")
+		return
+	}
+	defer im.Close()
+	if _, exists := validImageFormats[imFileHeader.Header["Content-Type"][0]]; !exists {
+		w.WriteHeader(400)
+		fmt.Fprint(w, "Image file format not valid.")
+		return
+	}
+	if imFileHeader.Size/1000000 > 5 {
+		w.WriteHeader(413)
+		fmt.Fprint(w, "La imagen pesa más de 5 MB.")
+		return
+	}
+	imageURL := "/" + parseUserEmail(userId)
+	// ensure dir exists and create final file
+	err2 := os.MkdirAll("static"+imageURL, os.ModePerm)
+	if(err2 != nil){
+		println(err2.Error())
+	}
+	imageURL += "/"
+	filename := imFileHeader.Filename
+	filename = strings.Replace(filename, " ", "", -1)
+	additionalNum := ""
+	for fileExists("static" + imageURL + additionalNum + filename) {
+		if additionalNum == "" {
+			additionalNum = "1"
+		} else {
+			num, _ := strconv.Atoi(additionalNum)
+			num++
+			additionalNum = strconv.Itoa(num)
+		}
+	}
+	imageURL += additionalNum + filename
+	file, err := os.Create("static" + imageURL,)
+	if err != nil {
+		println(err.Error())
+		w.WriteHeader(500)
+		fmt.Fprint(w, "Error creating file.")
+		return
+	}
+	defer file.Close()
+	// write image file to dir
+	_, err = io.Copy(file, im)
+	if err != nil {
+		println(err.Error())
+		w.WriteHeader(500)
+		fmt.Fprint(w, "Error copying the image file.")
+		return
+	}
+	habit := models.Habit{
+		Title:     	r.MultipartForm.Value["title"][0],
+		Type: 		hType,
+		Difficulty: difficulty,
+		UserId:		userId,
+		Image:		"/images" + imageURL,
+		Score:      0,
+	}
+	result, err := controller.habitsDB.Insert(habit)
+	if err != nil {
+		println(err.Error())
+		_ = os.Remove(("/static" + imageURL))
+		w.WriteHeader(500)
+		fmt.Fprint(w, "Error uploading image to database.")
+		return
+	}
+	habit.ID = result.InsertedID.(primitive.ObjectID)
+	w.Header().Add("Content-Type", "application/json")
+	encoder := json.NewEncoder(w)
+	encoder.Encode(habit)
+}
+func fileExists(fileName string) bool {
+	_, err := os.Stat(fileName)
+	return !os.IsNotExist(err)
+}
+func parseUserEmail(email string) string {
+	emailParsed := ""
+	for i:=0; i < len(email); i++ {
+		if(email[i] == '@') {
+			break
+		} 
+		emailParsed += string(email[i])
+	}
+	return emailParsed
+}
+// EditHabit serves to receive a PUT request to modify an existing habit
+func (controller *HabitsController) EditHabit(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	if len(id) < 1 {
+		w.WriteHeader(500)
+		fmt.Fprint(w, "No ID was provided.")
+		return
+	}
+	habitID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		println(err.Error())
+		w.WriteHeader(500)
+		fmt.Fprint(w, "Error while intepreting the habit ID.")
+		return
+	}
+	// Parse url-encoded data
+	err = r.ParseForm()
+	if err != nil {
+		println(err.Error())
+		w.WriteHeader(500)
+		fmt.Fprint(w, "Error while intepreting the data provided.")
+		return
+	}
+	if len(r.Form["title"]) == 0 {
+		w.WriteHeader(400)
+		fmt.Fprint(w, "Missing title.")
+		return
+	}
+	if len(r.Form["type"]) == 0 {
+		w.WriteHeader(400)
+		fmt.Fprint(w, "Missing type.")
+		return
+	}
+	if len(r.Form["difficulty"]) == 0 {
+		w.WriteHeader(400)
+		fmt.Fprint(w, "Missing difficulty.")
+		return
+	}
+	// get age from image
+	hType, err := strconv.Atoi(r.Form["type"][0])
+	if (err != nil || hType < -1 || hType > 1){
+		println(err.Error())
+		w.WriteHeader(400)
+		fmt.Fprint(w, "Either type was not a number or is not within the permitted range.")
+		return
+	}
+	difficulty, err := strconv.Atoi(r.Form["difficulty"][0])
+	if (err != nil){
+		println(err.Error())
+		w.WriteHeader(400)
+		fmt.Fprint(w, "Difficulty was not a number.")
+		return
+	}
+	filter := bson.D{{"_id", habitID}}
+	updateDoc := bson.D{{"$set", bson.D{{"title", r.Form["title"][0]},
+		{"type", hType},
+		{"difficulty", difficulty}}}}
+	_, err = controller.habitsDB.UpdateOne(filter, updateDoc)
+	if err != nil {
+		println(err.Error())
+		w.WriteHeader(500)
+		fmt.Fprint(w, "Error updating data in the database.")
+		return
+	}
+	var result models.Habit
+	err = controller.habitsDB.GetByID(filter, &result)
+	if err != nil {
+		println(err.Error())
+		w.WriteHeader(500)
+		fmt.Fprint(w, "Error retrieving new values.")
+		return
+	}
+	
+	w.Header().Add("Content-Type", "application/json")
+	encoder := json.NewEncoder(w)
+	encoder.Encode(result)
+}
+
+func (controller *HabitsController) DeleteHabit(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	if len(id) < 1 {
+		w.WriteHeader(500)
+		fmt.Fprint(w, "No ID was provided.")
+		return
+	}
+	habitID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		println(err.Error())
+		w.WriteHeader(500)
+		fmt.Fprint(w, "Error while intepreting the habit ID.")
+		return
+	}
+	filter := bson.D{{"_id", habitID}}
+	deleteResult, err := controller.habitsDB.DeleteOne(filter)
+	if err != nil {
+		w.WriteHeader(500)
+		fmt.Fprint(w, "Error while deleting habit.")
+		return
+	}
+	w.Header().Add("Content-Type", "application/json")
+	encoder := json.NewEncoder(w)
+	encoder.Encode(deleteResult)
+}
+
+func (controller * HabitsController) SumPoints(w http.ResponseWriter, r * http.Request){
+	vars := mux.Vars(r)
+	id := vars["id"]
+	if len(id) < 1 {
+		w.WriteHeader(500)
+		fmt.Fprint(w, "No ID was provided.")
+		return
+	}
+	habitID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		println(err.Error())
+		w.WriteHeader(500)
+		fmt.Fprint(w, "Error while intepreting the habit ID.")
+		return
+	}
+	if len(models.Habits["difficulty"]) == 0 {
+		w.WriteHeader(400)
+		fmt.Fprint(w, "Missing difficulty.")
+		return
+	}
+	//Set difficulty
+	difficulty := 0
+	if(models.Habit[Difficulty] == "Easy"){difficulty = 2}
+	if(models.Habit[Difficulty] == "Medium"){difficulty = 3}
+	if(models.Habit[Difficulty] == "Hard"){difficulty = 5}
+	//get score as int
+	score, err := strconv.Atoi(models.Habit[Score])
+	if err == nil{ fmt.Println(score)}
+	//sumar difficulty a score
+	if(score < 0){ // es rojo
+		if(models.Habit["Type"] == "good"){
+			score += difficulty
+		}
+		if(models.Habit["Type"] == "bad"){
+			score -= difficulty*2
+		}
+	}
+	else if(score >= 0 && score < 10){ // es naranja
+		if(models.Habit["Type"] == "good"){
+			score += difficulty
+		}
+		if(models.Habit["Type"] == "bad"){
+			score -= difficulty*1.5
+		}
+	}
+	else if(score >= 10 && score < 40){ // es amarillo 
+		if(models.Habit["Type"] == "good"){
+			score += difficulty
+		}
+		if(models.Habit["Type"] == "bad"){
+			score -= difficulty
+		}
+	}
+	else if(score >= 40 && score < 50){
+		if(models.Habit["Type"] == "good"){
+			score += difficulty*1.5
+		}
+		if(models.Habit["Type"] == "bad"){
+			score -= difficulty
+		}
+	}
+	else{
+		if(models.Habit["Type"] == "good"){
+			score += difficulty*2
+		}
+		if(models.Habit["Type"] == "bad"){
+			score -= difficulty
+		}
+	}
+	score = score + ""
 }
--- habits/db/habitDb.go
+++ habits/db/habitDb.go
@@ -1,22 +1,49 @@
 package db
-
 import (
 	"context"
-
+	"fmt"
 	"github.com/LuisPalominoTrevilla/TaskManagerSystemBackend/models"
 	"github.com/mongodb/mongo-go-driver/bson"
 	"github.com/mongodb/mongo-go-driver/mongo"
 )
-
 // UserDB represents the habits collection
 type HabitsDB struct {
 	Habits *mongo.Collection
 }
-
 func (db *HabitsDB) GetByID(filter bson.D, result *models.Habit) error {
 	return db.Habits.FindOne(context.TODO(), filter).Decode(&result)
 }
-
 func (db *HabitsDB) Insert(habit models.Habit) (*mongo.InsertOneResult, error) {
 	return db.Habits.InsertOne(context.TODO(), habit)
+}
+func (db *HabitsDB) Get(filter bson.D) ([]*models.Habit, error) {
+	cur, err := db.Habits.Find(context.TODO(), filter)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	var result []*models.Habit
+	for cur.Next(context.TODO()) {
+		var habit models.Habit
+		err := cur.Decode(&habit)
+		if err != nil {
+			fmt.Println(err)
+			return nil, err
+		}
+		result = append(result, &habit)
+	}
+	return result, nil
+}
+// UpdateOne updates a document in the database
+func (db *HabitsDB) UpdateOne(filter bson.D, update bson.D) (*mongo.UpdateResult, error) {
+	return db.Habits.UpdateOne(context.TODO(), filter, update)
+}
+
+// DeleteOne updates a document in the database
+func (db *HabitsDB) DeleteOne(filter bson.D) (*mongo.DeleteResult, error) {
+	return db.Habits.DeleteOne(context.TODO(), filter)
+}
+
+func (db *HabitsDB) SumPoints(filter bson.D, update bson.D)(*mongo.UpdateResult, error){
+	return db.Habits.SumPoints(context.TODO(), filter, update)
 }
