c98798c04d6855e6fc9c21a0e23a6f5bc8bb0dab
Luis Palomino Trevilla <luispalominot3@gmail.com>
2019-05-14 16:10:55 -0500

    Created tasks due today report query

--- reports/routes/modules/reports.js
+++ reports/routes/modules/reports.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const moment = require('moment-timezone');
-const request = require('request');
+const serviceFactory = require('../../db/servicesFactory');
 const router = express.Router();
 
 moment.tz.setDefault('America/Mexico_City');
 
 router.get('/', (req, res) => {
     res.send('Hello from reports');
+});
+
+router.get('/users/:userId', (req, res) => {
+    const accounts = serviceFactory('accounts');
+    const userId = req.params.userId;
+    accounts.getTasksDueToday(userId)
+        .then(tasks => {
+            res.status(200).json(tasks);
+        })
+        .catch(err => {
+            res.status(err.error).send(err);
+        })
 });
 
 module.exports = router;
